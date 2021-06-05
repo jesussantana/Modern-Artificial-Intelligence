e73b6777ffe0832129e801ee6feb186bb7621c94
jesussantana <jesus@esmarketingdigital.com>
2021-06-05 12:18:07 +0200

    Project Otganization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='5 hands-on projects and unleash the power of AI to solve real-world business problems',
+    author='Jesus Santana',
+    license='MIT',
+)
